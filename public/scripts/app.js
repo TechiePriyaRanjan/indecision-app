662c1b06495010ebc3d9938cb0942f705d202188
TechiePriyaRanjan <hackerswanted2012@gmail.com>
2018-01-09 22:33:22 +0530

    Indecision App barebones setup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,42 @@
 console.log("Wow! I'm learning React.js with Andrew Mead. 😉");
 
 // JSX = Javascript XML
+
+// Challenge Two
+var app = {
+    title: "Indecision App",
+    subtitle: "This is some subtitle"
+};
+
+var studentInfo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h2",
+        null,
+        app.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "List 1"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "List 2"
+        )
+    )
+);
+
 var template = React.createElement(
     "div",
     null,
@@ -32,6 +68,35 @@ var template = React.createElement(
     )
 );
 
+// Challange one
+var user = {
+    name: "Priya Ranjan Dubey",
+    age: 25,
+    location: "Patna, Bihar"
+};
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
+);
+
 // Getting the DOM element by its ID
 var AppRoot = document.getElementById('app');
 
@@ -39,4 +104,4 @@ var AppRoot = document.getElementById('app');
 ReactDOM.render() method takes two arguments, 1st the JSX
  and the second the DOM element we want to render
 */
-ReactDOM.render(template, AppRoot);
+ReactDOM.render(studentInfo, AppRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,25 @@
 console.log("Wow! I'm learning React.js with Andrew Mead. 😉");
 
 // JSX = Javascript XML
+
+// Challenge Two
+var app = {
+    title: "Indecision App",
+    subtitle: "This is some subtitle"
+};
+
+var studentInfo = 
+    <div>
+        <h2>{app.title}</h2>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>List 1</li>
+            <li>List 2</li>
+        </ol>
+    </div>
+;
+
+
 var template =
     <div>
         <h1>Wow! I'm learning React.js with Andrew Mead.😉</h1>
@@ -13,6 +32,21 @@ var template =
     </div>
 ;
 
+// Challange one
+var user = {
+        name : "Priya Ranjan Dubey",
+        age : 25,
+        location : "Patna, Bihar"
+};
+
+var templateTwo = 
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+;
+
 // Getting the DOM element by its ID
 var AppRoot = document.getElementById('app');
 
@@ -20,4 +54,4 @@ var AppRoot = document.getElementById('app');
 ReactDOM.render() method takes two arguments, 1st the JSX
  and the second the DOM element we want to render
 */
-ReactDOM.render(template, AppRoot);
+ReactDOM.render(studentInfo, AppRoot);
